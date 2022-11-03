825bf12f8fcc5b4c04cea1bfc8cd8c07d40ce25c
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-03 10:02:22 +0000

    feat: add contact route path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 
 
@@ -12,6 +13,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
+              <Route path="contact" element={<Contact />} />
             </Route>
           </Routes>
         </BrowserRouter>
