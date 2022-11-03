8af77a1c66b7eb8c23774d039e6d820b0c778cc8
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-03 12:38:59 +0000

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,12 +13,10 @@ export default function Footer() {
           <img src={I4G} alt="I4G logo" />
         </div>
 
-        <div className="py-4 flex flex-col text-center gap-5 sm:hidden">
+        <div className="border-t py-4 flex flex-col mx-4 mb-4 gap-5 sm:hidden">
+          <img src={zuri} className="w-40 object-contain" alt="zuri internship logo" />
           <span className="text-md text-gray-500">HNG Internship 9 Frontend Task</span>
-          <div className="flex justify-between px-4">
-            <img src={zuri} className="w-24 object-contain" alt="zuri internship logo" />
-            <img src={I4G} className="w-24 object-contain" alt="I4G logo" />
-          </div>
+          <img src={I4G} className="w-28 object-contain" alt="I4G logo" />
         </div>
       </footer>
     </>
