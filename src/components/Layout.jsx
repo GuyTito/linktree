35f72a68ed15a972532e9147cae6eba8099cb643
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-03 12:05:04 +0000

    add footer component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-
+import Footer from './Footer';
 
 export default function Layout() {
 
@@ -9,6 +9,7 @@ export default function Layout() {
 
       <Outlet />
 
+      <Footer />
     </>
   )
 }
