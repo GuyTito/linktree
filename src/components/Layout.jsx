e9b842608f4369f7e160bcd10f6c49ed3714a270
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-04 16:50:22 +0000

    scroll to top on navigation change

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from './Footer';
 
 export default function Layout() {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      // behavior: "instant", // Optional if you want to skip the scrolling animation
+    });
+  }, [pathname]);
 
   return (
     <>
