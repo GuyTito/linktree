f5832e25400403334e463cf4794349f3b4017ece
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-11-04 10:55:40 +0000

    style: focus ring on submit button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -27,11 +27,11 @@ export default function Contact() {
             <textarea id="message" placeholder="Send me a message and I'll reply you as soon as possible..." className="input h-[132px]"></textarea>
           </div>
           <div className="flex items-center mb-8">
-            <input type="checkbox" id="checkbox" className="w-5 h-5 border-gray-300 rounded-[6px] checked:border-blue-600 checked:hover:border-blue-600 focus:ring focus:ring-offset-0 focus:ring-blue-200 focus:border-primary text-gray-50 checked:bg-check" />
+            <input type="checkbox" id="checkbox" className="w-5 h-5 border-gray-300 rounded-[6px] checked:border-blue-600 checked:hover:border-blue-600 focus:ring-offset-0 focus-ring text-gray-50 checked:bg-check" />
             <label htmlFor="checkbox" className="ml-3 text-gray-600 text-[16px]">You agree to providing your data to Kofi Quist who may contact you.</label>
           </div>
           <div>
-            <input type="submit" id="btn__submit" value="Send message" className="bg-blue-600 rounded-lg py-3 w-full text-white cursor-pointer" />
+            <input type="submit" id="btn__submit" value="Send message" className="bg-blue-600 rounded-lg py-3 w-full text-white focus-ring cursor-pointer" />
           </div>
         </form>
       </main>
